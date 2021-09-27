004c68dfb6e5038125f4cefc28de03e557cbeae2
muchensthughs <muchen498@gmail.com>
2021-09-27 10:44:08 -0700

    Create SpringBoot project

---
+++ myTrial-api/src/main/java/com/cmpe138/mytrial/MyTrialApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe138.mytrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTrialApplication.class, args);
+	}
+
+}
