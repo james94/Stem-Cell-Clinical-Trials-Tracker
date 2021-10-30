74a77db95d0e2bbe4469689ba4b4ce20d8f573e7
TingHsuanYang <evan.yang@sisu.edu>
2021-10-29 19:03:10 -0700

    create model, org service and org repo

---
+++ myTrial-api/src/main/java/com/cmpe138/mytrial/model/Organization.java
@@ -0,0 +1,20 @@
+package com.cmpe138.mytrial.model;
+
+public class Organization {
+
+	public String org_name;
+
+	public String getOrg_name() {
+		return org_name;
+	}
+
+	public void setOrg_name(String org_name) {
+		this.org_name = org_name;
+	}
+
+	@Override
+	public String toString() {
+		return "Organization [org_name=" + org_name + "]";
+	}
+
+}
