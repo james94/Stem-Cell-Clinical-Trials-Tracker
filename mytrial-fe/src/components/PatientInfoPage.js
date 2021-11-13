f9b048cdf15d894a7c5adaf63710746ca00ed663
Evan Yang <evan.yang@sisu.edu>
2021-11-12 23:59:52 -0800

    modify patientInfoPage, create link to trial and researcher

--- mytrial-fe/src/components/PatientInfoPage.js
+++ mytrial-fe/src/components/PatientInfoPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PatientService from '../service/PatientService';
 
 class PatientInfoPage extends React.Component {
@@ -7,87 +7,70 @@ class PatientInfoPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            patient_id: "",
-            disease: "",
-            phase: "",
-            p_status: "",
-            // fk
-            trial_id: "",
-            p_username: "",
-            p_password: "",
-            // fk
-            // researcher_id: "",
-            researcher_id: "000000042"
+            researcher_id: "000000052",
+            patientToShow: {},
+            trialToShow: {},
+            researcherToShow: {}
             // researcher_id: localStorage.getItem('id')
         };
-        // this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    // handleChange = (e) => {
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     })
-    // }
-
-    // handleSubmit = (e) => {
-    //     alert('Submit ' + this.state);
-    //     let discussion = {
-    //         df_name: this.state.df_name,
-    //         details: this.state.details,
-    //         researcher_id: this.state.researcher_id
-    //     }
-    //     DiscussionService.createDiscussion(discussion)
-    //         .then(data => {
-    //             console.log(data)
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }
 
     render() {
         return (
             <div>
-                {/* patient list */}
-                {this.state.disease}, {this.state.phase}, {this.state.p_status}, {this.state.trial_id},{this.state.researcher_id}
-                {/* create new discussion */}
-                {/* update phase */}
-                {/* update disease */}
-                {/* update status */}
-                {/* change trial */}
-                {/* <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="df_name" value={this.state.df_name} onChange={this.handleChange} /><br />
-                    <textarea name="details" value={this.state.details} onChange={this.handleChange} />
-                    <input type="submit" value="Submit" />
-                </form> */}
+                <h2>Patient Data</h2>
+                <pre>
+                    { }
+                    {JSON.stringify(this.state.patientToShow, null, 2)}
+                </pre>
+                {/* go to disease page */}
 
-                {/* private String patient_id;
-                private String disease;
-                private String phase;
-                private String p_status;
-                // fk
-                private String trial_id;
-                private String p_username;
-                private String p_password;
-                // fk
-                private String researcher_id; */}
+                <hr />
+                {/* go to trial page */}
+                <h2>Trial Data</h2>
+                {
+                    <Link to={{ pathname: `/trial/${this.state.trialToShow.trial_id}` }}>{this.state.trialToShow.title}</Link>
+                }
+                <pre>
+                    {JSON.stringify(this.state.trialToShow, null, 2)}
+                </pre>
+                <hr />
+                {/* go to researcher page */}
+                <h2>Researcher Data</h2>
+                {
+                    <Link to={{ pathname: `/researcher/${this.state.researcherToShow.researcher_id}` }}>{this.state.researcherToShow.r_name}</Link>
+                }
+                <pre>
+                    {JSON.stringify(this.state.researcherToShow, null, 2)}
+                </pre>
             </div>
         )
     }
 
     componentDidMount() {
-        console.log('asafdasfas');
         let config = {
             headers: {
                 researcher_id: "000000052"
             }
         };
+        console.log('aaa:' + this.state.trialToShow.title)
+        // let dataToShow = {
+        //     patient_id: "",
+        //     disease: "",
+        //     phase: "",
+        //     p_status: "",
+        //     trial: {},
+        //     researcher: {}
+        // }
         // show discussion list
         PatientService.getPatientById(this.props.match.params.id, config)
             .then(data => {
-                console.log(data)
-                this.setState(data.data);
+                // this.setState({ ...data.data });
+                var { p_username, p_password, trial, researcher, ...patientToShow } = data.data;
+                this.setState({ patientToShow: patientToShow });
+                this.setState({ trialToShow: data.data.trial });
+                var { r_username, r_password, ...researcherToShow } = data.data.researcher;
+                this.setState({ researcherToShow: researcherToShow });
             })
             .catch(err => {
                 console.log(err)
