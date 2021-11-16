033711fbdd21ba4381e3fdd71036b7d67a9a986a
James Medel <jamesmedel94@gmail.com>
2021-11-15 17:38:38 -0800

    Added SpringBoot Backend for Grant Controller, Service, Repo
    Just need to test this functionality specifically for get all grants, so the researcher can view a list of all grants from their portal by clicking a button that will take them to another page.

---
+++ myTrial-api/src/main/java/com/cmpe138/mytrial/controller/GrantController.java
@@ -0,0 +1,68 @@
+package com.cmpe138.mytrial.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cmpe138.mytrial.model.CTGrant;
+import com.cmpe138.mytrial.service.GrantService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@CrossOrigin
+@RestController
+public class GrantController {
+
+    @Autowired
+    private GrantService grantService;
+
+    @GetMapping("/grants")
+    public List<CTGrant> getGrants(@RequestHeader(value="id", required=false) String researcher_id,
+            @RequestHeader(value="list_all", required=false) boolean list_all,
+            @RequestParam(value="grant_number", required=false) String grant_number,
+            @RequestParam(value="trial_id", required=false) String trial_id,
+            @RequestParam(value="disease_focus", required=false) String disease_focus,
+            @RequestParam(value="institution", required=false) String institution,
+            @RequestParam(value="stem_cell_use", required=false) String stem_cell_use) {
+        if((!researcher_id.isEmpty()) && list_all == true) {
+            return grantService.getAll();
+        } else if(!grant_number.isEmpty()) {
+            List<CTGrant> res = new ArrayList<>();
+            res.add(grantService.getGrantByNumber(grant_number));
+            return res;
+        } else if(!trial_id.isEmpty()) {
+            return grantService.getGrantsByTrialId(trial_id);
+        } else if(!disease_focus.isEmpty()) {
+            return grantService.getGrantsByDiseaseFocus(disease_focus);
+        } else if(!institution.isEmpty()) {
+            return grantService.getGrantsByInstitution(institution);
+        } else if(!stem_cell_use.isEmpty()) {
+            return grantService.getGrantsByStemCellUse(stem_cell_use);
+        } else {
+            return grantService.getGrantsByResearcherId(researcher_id);
+        }
+    }
+
+    // TODO: Add Grant
+    @PostMapping("/addgrant")
+    public ResponseEntity<String> addGrant(@RequestHeader(value="id") String researcher_id, @RequestBody ObjectNode grant) {
+        try {
+            grantService.createGrant(researcher_id, grant);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>("Grant creation failed!", HttpStatus.BAD_REQUEST);
+        }
+        System.out.println("Grant creation success!");
+        return new ResponseEntity<>("Grant creation success!", HttpStatus.OK);
+    }
+}
---
+++ myTrial-api/src/main/java/com/cmpe138/mytrial/service/impl/GrantServiceImpl.java
@@ -0,0 +1,71 @@
+package com.cmpe138.mytrial.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cmpe138.mytrial.model.CTGrant;
+import com.cmpe138.mytrial.repository.GrantRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@Component
+public class GrantServiceImpl implements GrantService {
+
+    @Autowired
+    GrantRepository grantRepository;
+
+    public List<CTGrant> getAll() {
+        return grantRepository.findAll();
+    }
+    
+    public CTGrant getGrantByNumber(String grant_number) {
+        return grantRepository.findByGrantNumber(grant_number);
+    }
+
+    public List<CTGrant> getGrantsByTrialId(String trial_id) {
+        return grantRepository.findByTrialId(trial_id);
+    }
+
+    public List<CTGrant> getGrantsByDiseaseFocus(String disease_focus) {
+        return grantRepository.findByDiseaseFocus(disease_focus);
+    }
+
+    public List<CTGrant> getGrantsByInstitution(String institution) {
+        return grantRepository.findByInstitution(institution);
+    }
+
+    public List<CTGrant> getGrantsByStemCellUse(String stem_cell_use) {
+        return grantRepository.findByStemCellUse(stem_cell_use);
+    }
+
+    public List<CTGrant> getGrantsByResearcherId(String researcher_id) {
+        return grantRepository.findByResearcherId(researcher_id);
+    }
+
+    public void createGrant(String researcher_id, ObjectNode grant) {
+        // grant number primary key [12 letters/numbers - 5 numbers]
+        // String first_half = getRandomString(12);
+        // String second_half = String.valueOf((int)(Math.random() * 99999));
+        String grant_number = getRandomString(12) + "-" + String.valueOf((int)(Math.random() * 99999));
+        while(getGrantByNumber(grant_number) != null) {
+            grant_number = getRandomString(12) + "-" + String.valueOf((int)(Math.random() * 99999));
+        }
+        grantRepository.addGrant(researcher_id, grant);
+    }
+
+    protected String getRandomString(int required_length) {
+        String sampleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder sample = new StringBuilder();
+        Random rand = new Random();
+        while(sample.length() < required_length) { // length of random str
+            int index = (int)(rand.nextFloat() * sampleChars.length());
+            sample.append(sampleChars.charAt(index));
+        }
+        String sampleStr = sample.toString();
+        return sampleStr;
+    }
+}
+
