abf2c76c46432e4a535af3a78d05380d28e4433d
James Medel <jamesmedel94@gmail.com>
2021-11-16 13:44:03 -0800

    Added MyTrial Functionality to Patient's Profile
    Now when a patient logs into their account, they can click on the MyTrial button and it will take them to a page that lists their trial, they can click that trial to see the trial info page.
    
    Updated some code in ReactJS frontend and SpringBoot backend.
    
    Verified it works with docker containers deployed.

--- myTrial-api/src/main/java/com/cmpe138/mytrial/service/impl/MyTrialServiceImpl.java
+++ myTrial-api/src/main/java/com/cmpe138/mytrial/service/impl/MyTrialServiceImpl.java
@@ -35,6 +35,11 @@ public class MyTrialServiceImpl implements MyTrialService {
 	public List<Trial> getTrialsByResearcherId(String researcher_id) {
 		return trialRepository.findByResearcherId(researcher_id);
 	}
+
+	@Override
+	public List<Trial> getTrialsByPatientId(String patient_id) {
+		return trialRepository.findByPatientId(patient_id);
+	}
 	
 	@Override
 	@Transactional
--- mytrial-fe/src/components/PatientHomePage.js
+++ mytrial-fe/src/components/PatientHomePage.js
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 const PatientHomePage = () => {
     return (
-        <div>
-            <button>My Trial</button>
+        <div style={{ marginLeft: "10px" }}>
+            <button><Link to='/trials'>My Trial</Link></button>
             <button><Link to={'/pwatchlists/' + localStorage.getItem('id')}>Watchlists</Link></button>
         </div>
     )
